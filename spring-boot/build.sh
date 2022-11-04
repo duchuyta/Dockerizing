4e41bb3bc283d560fdb68470e32ff3982c21a575
Duc Huy Ta <d.huyta@gmail.com>
2022-11-04 17:22:49 +0700

    Create build.sh

---
+++ spring-boot/build.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+while getopts r:u:p:i:t: flag
+do
+    case "${flag}" in
+        r) REGISTRY=${OPTARG};;
+        u) REGISTRY_USERNAME=${OPTARG};;
+        p) REGISTRY_PASSWORD=${OPTARG};;
+        i) IMAGE=${OPTARG};;
+        t) TAG=${OPTARG};;
+    esac
+done
+
+docker login -u "$REGISTRY_USERNAME" -p "$REGISTRY_PASSWORD" $REGISTRY
+
+docker build -t $REGISTRY:$TAG docker/
+docker tag $IMAGE:$TAG $REGISTRY/$IMAGE:$TAG
+docker push $REGISTRY$IMAGE:$TAG
+
+envsubst < docker/docker-compose.yml > docker-compose.yml
