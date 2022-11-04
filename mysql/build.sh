5e17e4813dd84d2d714b8206eb99a0955511438b
Duc Huy Ta <d.huyta@gmail.com>
2022-11-04 16:49:25 +0700

    Update build.sh

--- mysql/build.sh
+++ mysql/build.sh
@@ -2,5 +2,5 @@
 docker build -t mysql:5.7.40 .
 
 docker login <registry>
-docker tag mysql:5.7.40 <registry/mysql:5.7.40>
-docker push <registry/mysql:5.7.40>
+docker tag mysql:5.7.40 <registry>/mysql:5.7.40
+docker push <registry>/mysql:5.7.40
